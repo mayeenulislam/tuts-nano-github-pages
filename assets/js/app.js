afa139870dadee43c2a96268d98efb9fc894e79e
Mayeenul Islam <wz.islam@gmail.com>
2016-11-14 11:39:55 +0600

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Just a Demo JavaScript file
+ * 
+ * @project GithubPages Demo
+ * @author  nanodesigns
+ */
+
+// Select the <html> tag
+var elem_html = document.documentElement;
+
+// Replace the 'no-js' class with 'js'
+elem_html.className = "js";
